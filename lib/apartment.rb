cdc2cd9f954dbc8e7bf64830ff83dd9fe4fe59b7
Michael Foy <foymikek@gmail.com>
2020-10-20 09:15:31 -0600

    Add passing test_it_has_attirbutes

---
+++ lib/apartment.rb
@@ -0,0 +1,10 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+  def initialize(data)
+    @number = data[:number]
+    @monthly_rent = data[:monthly_rent]
+    @bathrooms = data[:bathrooms]
+    @bedrooms = data[:bedrooms]
+    @renter = data[:renter]
+  end
+end
