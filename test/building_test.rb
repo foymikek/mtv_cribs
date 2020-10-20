7fbb779fde7c724f6b8d942146c9b0d14f2b9cb3
Michael Foy <foymikek@gmail.com>
2020-10-20 09:24:46 -0600

    Add working test_building_can_add_units

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,8 +7,19 @@ require "./lib/building"
 class BuildingTest < Minitest::Test
   def test_it_has_attirbutes
     building = Building.new
-    
+
     assert_equal [], building.units
     assert_equal [], building.renters
+  end
+
+  def test_building_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    expected = [unit1, unit2]
+    assert_equal expected, building.units
   end
 end
